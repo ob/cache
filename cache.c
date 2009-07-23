fa9b25efb981fc42158680c1e458a09a3085ce9a
Oscar Bonilla <ob@oblib.net>
2009-07-22 23:43:50 -0700

    Initial commit, copied first working version of files.

---
+++ cache.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/types.h>
+
+#define	SAMPLE		10
+#define	CACHE_MIN	4 * 1024
+#define	CACHE_MAX	512 * 1024 * 1024
+
+typedef u_int8_t	u8;
+typedef	u_int32_t	u32;
+typedef u_int64_t	u64;
+
+u8			x[CACHE_MAX];
+
+double
+timestamp(void)
+{
+    struct timeval	tv;
+    gettimeofday(&tv, 0);
+    return (tv.tv_sec + (tv.tv_usec/1000000.0));
+}
+
+int
+main(int ac, char **av)
+{
+    u32 register	i, j, k, stride, temp;
+    u32	steps, limit, tsteps, csize;
+    double	sec0, sec;
+
+    for (csize=CACHE_MIN; csize <= CACHE_MAX; csize*=2) {
+	for (stride=1; stride <= csize; stride=stride*2) {
+	    sec = 0.0;
+	    steps = 0;
+	    do {
+		sec0 = timestamp();
+		for (i=0; i < SAMPLE; i++) {
+		    for (j=0; j < csize; j+=stride) {
+			x[j] += 1;
+		    }
+		}
+		steps++;
+		sec += timestamp() - sec0;
+	    } while (sec < 1.0);
+
+	    tsteps = 0;
+	    do {
+		sec0 = timestamp();
+		for (i=0; i < SAMPLE; i++) {
+		    for (j=0; j < csize; j+=stride) {
+			temp += j;
+		    }
+		}
+		tsteps++;
+		sec -= timestamp() - sec0;
+	    } while (tsteps < steps);
+	    printf("%u\t%u\t%.1lf\n",
+		   stride,
+		   csize,
+		   (sec * 1000000000.0) /
+		   (SAMPLE * steps * (csize / stride)));
+	    fflush(stdout);
+	}
+	printf("\n");
+	fflush(stdout);
+    }
+}
+
+/*
+ * Local Variables: **
+ * c-file-style: "cc-mode" **
+ * End: **
+ */
---
+++ plot
@@ -0,0 +1,27 @@
+#set term x11
+set ticslevel 0
+set xrange [] reverse
+set xtics (2, 8, 32, 128, 512, "2K" 2048, "8K" 8192, "32K" 32768, "128K" 131072, \
+    "512K" 524288, "2M" 2097152, "8M" 8388608, "32M" 33554432, "128M" 134217728, \
+    "512M" 536870912)
+set ytics (2, 8, 32, 128, 512, "2K" 2048, "8K" 8192, "32K" 32768, "128K" 131072, \
+    "512K" 524288, "2M" 2097152, "8M" 8388608, "32M" 33554432, "128M" 134217728, \
+    "512M" 536870912)
+set grid
+unset key
+set title "Cache Latency"
+set xlabel "Stride (bytes)"
+set ylabel "Buffer Size (bytes)"
+set zlabel "Time (ns)"
+#set view 57,13
+set view 70,30
+set pm3d at s hidden3d 100
+set style line 100 lt 5 lw 0.5
+unset surf
+set log xy # add z to use log scale for time
+set log z
+splot "COLD4" with lines
+
+# Local Variables: **
+# mode: gnuplot **
+# End: **
