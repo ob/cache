23db42044d9b3a610a4cd31529d4bd9b656fdb91
Oscar Bonilla <ob@oblib.net>
2009-07-27 13:32:25 -0700

    Add option parsing and implement both random and sequential access.
    Also, make strides start from 1 to better match Saavedra.

--- cache.c
+++ cache.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
+#include <getopt.h>
 #include <sys/time.h>
 #include <sys/types.h>
+
+typedef	u_int8_t	u8;
+typedef	u_int32_t	u32;
+typedef u_int64_t	u64;
 
 #define	SAMPLE		10
 #define	CACHE_MIN	1024
 #define	CACHE_MAX	256 * 1024 * 1024 /* 1 GB! */
 
-typedef	u_int8_t	u8;
-typedef	u_int32_t	u32;
-typedef u_int64_t	u64;
+/* Getopt */
+static struct option longopts[] = {
+    { "random",		no_argument,		NULL,		'r' },
+    { "sequential",	no_argument,		NULL,		's' },
+    { "buffer",		required_argument,	NULL,		'b' },
+    { NULL,		0,			NULL,		0 }
+};
+
+typedef enum {rand_access, seq_access} access_kind;
+static char	*names[3] = {"random", "sequential", 0};
+
 
 u32*	buffer[CACHE_MAX];
 
@@ -37,7 +50,7 @@ shuffle(u32 **buffer, u32 stride, u32 max)
     } else {
 	bzero(indices, CACHE_MAX);
     }
-    for (i = 0, j = 0; i < max; i+=stride, j++) indices[j] = i;
+    for (i = 1, j = 0; i <= max; i+=stride, j++) indices[j] = i;
 /* shuffle it */
      n = j;
      while (n > 1) {
@@ -55,19 +68,20 @@ shuffle(u32 **buffer, u32 stride, u32 max)
      return (&buffer[indices[0]]);
 }
 
-int
-main(int ac, char **av)
+void
+random_access(u32 cache_max)
 {
     u32 register	i, stride;
-    u32	steps, csize;
+    u32	steps, csize, limit;
     double	sec0, sec;
     u32 **start, **p;
 
-    for (csize=CACHE_MIN; csize <= CACHE_MAX; csize*=2) {
-	for (stride=1; stride <= csize; stride=stride*2) {
+    for (csize=CACHE_MIN; csize <= cache_max; csize*=2) {
+	for (stride=1; stride <= csize/2; stride=stride*2) {
 	    sec = 0.0;
+	    limit = csize - stride + 1;
 	    steps = 0;
-	    start = shuffle(buffer, stride, csize);
+	    start = shuffle(buffer, stride, limit);
 	    do {
 		sec0 = timestamp();
 		for (i=SAMPLE*stride; i > 0; i--) {
@@ -80,14 +94,97 @@ main(int ac, char **av)
 	    printf("%lu\t%lu\t%.1lf\n",
 		   stride * sizeof(u32),
 		   csize * sizeof(u32),
-		   (sec * 1000000000.0) /
-		   (SAMPLE * stride * steps * (csize / stride)));
+		   sec*1e9/(steps*SAMPLE*stride*((limit-1)/stride+1)));
 	    fflush(stdout);
 	}
 	printf("\n");
 	fflush(stdout);
     }
-    return 0;
+}
+
+void
+sequential_access(u32 cache_max)
+{
+    u32 register	i, j, stride;
+    u32 steps, csize, limit;
+    double	sec0, sec;
+
+    for (csize=CACHE_MIN; csize <= cache_max; csize*=2) {
+	for (stride=1; stride <= csize/2; stride=stride*2) {
+	    sec = 0.0;
+	    limit = csize - stride + 1;
+	    steps = 0;
+	    do {
+		sec0 = timestamp();
+		for (i=SAMPLE*stride; i > 0; i--) {
+		    for (j=1; j <= limit; j+=stride) {
+			buffer[j] = buffer[j] + 1;
+		    }
+		}
+		steps++;
+		sec += timestamp() - sec0;
+	    } while (sec < 1.0);
+
+	    printf("%lu\t%lu\t%.1lf\n",
+		   stride * sizeof(u32),
+		   csize * sizeof(u32),
+		   sec*1e9/(steps*SAMPLE*stride*((limit-1)/stride+1)));
+	    fflush(stdout);
+	}
+	printf("\n");
+	fflush(stdout);
+    }
+}
+
+int
+main(int ac, char **av)
+{
+    int	c;
+    u32	cache_size;
+    access_kind	kind;
+
+    cache_size = CACHE_MAX;
+    while ((c = getopt_long(ac, av, "rsc:", longopts, NULL)) != -1) {
+	switch (c) {
+	case 'r':
+	    kind = rand_access;
+	    break;
+	case 's':
+	    kind = seq_access;
+	    break;
+	case 'b':
+	    cache_size = strtol(optarg, 0, 10) / sizeof(u32);
+	    break;
+	default:
+usage:	    fprintf(stderr, "usage: %s [--random|--sequential]\n", av[0]);
+	    return (1);
+	    break;
+	}
+    }
+    if (cache_size < CACHE_MIN) {
+	fprintf(stderr, "%s: buffer needs to be at least %ld\n",
+		av[0], CACHE_MIN * sizeof(u32));
+	return (1);
+    }
+    if (cache_size > CACHE_MAX) {
+	fprintf(stderr, "%s: buffer needs to be less than %ld\n",
+		av[0], CACHE_MAX * sizeof(u32));
+	return (1);
+    }
+    printf("# doing %s access with a max buffer of %ld\n",
+	   names[kind], cache_size * sizeof(u32));
+    switch (kind) {
+    case rand_access:
+	    random_access(cache_size);
+	    break;
+    case seq_access:
+	    sequential_access(cache_size);
+	    break;
+    default:
+	goto usage;
+	break;
+    }
+    return (0);
 }
 
 /*
